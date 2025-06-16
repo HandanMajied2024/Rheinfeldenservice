ba4257e86337d62e0504e0ce044812a7e6be510d
Handan Majied <handan.majied1@gmail.com>
2025-06-16 13:00:34 +0200

    try a couple of new things then find out that its easier to make a website with AI

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ function sagHallo() {
     /*Zeigt ein kleines Popup-Fenster im
     Browser mit der Nachricht*/
 }
+
+function toggleMenu() {
+    const nav = document.getElementById("nav-menu");
+    nav.classList.toggle("open");
+}
