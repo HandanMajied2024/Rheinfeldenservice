8526b002790e145d94035999eb07777ddd97d82e
Handan Majied <handan.majied1@gmail.com>
2025-09-19 12:30:47 +0200

    Removed nested repo FirstWebsite

---
+++ FirstWebsite/script.js
@@ -0,0 +1,10 @@
+function sagHallo() {
+    alert("Hallo, TEST Test duuuuuh");
+    /*Zeigt ein kleines Popup-Fenster im
+    Browser mit der Nachricht*/
+}
+
+function toggleMenu() {
+    const nav = document.getElementById("nav-menu");
+    nav.classList.toggle("open");
+}
